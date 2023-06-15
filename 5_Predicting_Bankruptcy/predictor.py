44349b95a936dca158fe3ed920715f8c30546c47
ObiTheExplorer <rnoogbuagu@gmail.com>
2023-06-15 12:25:19 +0100

    changed the final model to gradient boosting classifier. I also re ran and saved the notebook again. Saved changed I made to the predictor module.

--- 5_Predicting_Bankruptcy/predictor.py
+++ 5_Predicting_Bankruptcy/predictor.py
@@ -2,7 +2,6 @@
 import gzip
 import json
 import pickle
-
 import pandas as pd
 
 
@@ -28,8 +27,11 @@ def make_predictions(data_filepath, model_filepath):
     # Load model
     with open(model_filepath,"rb") as f:
         model = pickle.load(f)
+        
     # Generate predictions
     y_test_pred = model.predict(X_test)
+    
     # Put predictions into Series with name "bankrupt", and same index as X_test
     y_test_pred = pd.Series(y_test_pred, index=X_test.index, name="bankrupt")
+    
     return y_test_pred
